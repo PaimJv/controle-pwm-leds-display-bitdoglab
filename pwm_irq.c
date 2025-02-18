78e8a4713ea7fdb4ac88b130f0f217385915ed51
João Victor Paim <156842254+PaimJv@users.noreply.github.com>
2025-02-18 16:05:09 -0300

    Update pwm_irq.c

--- pwm_irq.c
+++ pwm_irq.c
@@ -26,11 +26,25 @@
 #define JOY_THRESHOLD 100
 #define SQUARE_SIZE 8
 #define VELOCITY_DIVISOR 256  
+#define JOY_MAX 4095
 
+int ultimo_estado_pb;
+int ultimo_estado_a;
+uint intervalo_micro = 400000;
+
+ssd1306_t ssd;
+bool cor = true;
 uint16_t red_intensity;
 uint16_t blue_intensity;
 bool led_g_state = false;
 bool pwm_enabled = true;
+bool borda;
+volatile bool joystick_pb_pressed = false;  // Flag para interrupção
+
+// Função para mapear valores de um intervalo para outro
+int32_t map_value(int32_t value, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max) {
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; 
+}
 
 void init_pwm(uint gpio) {
     gpio_set_function(gpio, GPIO_FUNC_PWM);
@@ -44,33 +58,44 @@ void set_pwm_duty(uint gpio, uint16_t duty) {
     pwm_set_gpio_level(gpio, duty);
 }
 
-void gpio_irq_handler(uint gpio, uint32_t events) {
-    reset_usb_boot(0, 0);
+void irq_handler(uint gpio, uint32_t events) {
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+
+    if (gpio == JOYSTICK_PB && current_time - ultimo_estado_pb > intervalo_micro) {
+        ultimo_estado_pb = current_time;
+        borda = !borda;
+        led_g_state = !led_g_state;
+        gpio_put(LED_G, led_g_state);
+    }
+    if (gpio == Botao_A && current_time - ultimo_estado_a > intervalo_micro) {
+        ultimo_estado_a = current_time;
+        pwm_enabled = !pwm_enabled;
+        while (!gpio_get(Botao_A));
+    }
 }
 
 int main() {
-    gpio_init(botaoB);
-    gpio_set_dir(botaoB, GPIO_IN);
-    gpio_pull_up(botaoB);
-    gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-
     gpio_init(JOYSTICK_PB);
     gpio_set_dir(JOYSTICK_PB, GPIO_IN);
     gpio_pull_up(JOYSTICK_PB);
+    
     gpio_init(Botao_A);
     gpio_set_dir(Botao_A, GPIO_IN);
     gpio_pull_up(Botao_A);
 
+    gpio_set_irq_enabled_with_callback(JOYSTICK_PB, GPIO_IRQ_EDGE_FALL, true, &irq_handler);
+    gpio_set_irq_enabled(Botao_A, GPIO_IRQ_EDGE_FALL, true);
+    
     gpio_init(LED_G);
     gpio_set_dir(LED_G, GPIO_OUT);
 
+    // Inicializa I2C e display
     i2c_init(I2C_PORT, 400 * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    
-    ssd1306_t ssd;
+
     ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
     ssd1306_config(&ssd);
     ssd1306_send_data(&ssd);
@@ -85,55 +110,42 @@ int main() {
     init_pwm(LED_B);
 
     uint16_t adc_value_x, adc_value_y;
-
-    int16_t square_x = (HEIGHT - SQUARE_SIZE) / 2; // Alterado; quadrado centralizado
-    int16_t square_y = (WIDTH - SQUARE_SIZE) / 2; // Alterado; quadrado centralizado
-    bool cor = true;
+    int16_t square_x = (HEIGHT - SQUARE_SIZE) / 2;
+    int16_t square_y = (WIDTH - SQUARE_SIZE) / 2;
 
     while (true) {
-        adc_select_input(1);
+        // Leitura do joystick
+        adc_select_input(0); // Bitdoglab
+        // adc_select_input(1); // Simulador
         adc_value_x = adc_read();
-        adc_select_input(0);
+        adc_select_input(1);
+        // adc_select_input(0); // Simulador
         adc_value_y = adc_read();
 
-        // Calcular os deltas a partir do centro do joystick
-        int16_t delta_x = adc_value_x - JOY_CENTER;
-        int16_t delta_y = adc_value_y - JOY_CENTER;
+        int16_t square_x = map_value(adc_value_x, JOY_MAX, 0, 0, HEIGHT - SQUARE_SIZE);
+        int16_t square_y = map_value(adc_value_y, 0, JOY_MAX, 0, WIDTH - SQUARE_SIZE);
+
+        int16_t delta_x = adc_value_y - JOY_CENTER;
+        int16_t delta_y = adc_value_x - JOY_CENTER;
 
         if (abs(delta_x) > JOY_THRESHOLD) {
             square_x -= delta_x / VELOCITY_DIVISOR; 
         }
         if (abs(delta_y) > JOY_THRESHOLD) {
-            square_y -= delta_y / VELOCITY_DIVISOR; 
+            square_y += delta_y / VELOCITY_DIVISOR; 
         }
 
-        // Eixo X
-        if (square_x < 0) {
-            square_x = 0;
-        } else if (square_x > HEIGHT - SQUARE_SIZE) {
-            square_x = HEIGHT - SQUARE_SIZE;
-        }
+        // Limites do movimento
+        if (square_x < 0) square_x = 0;
+        else if (square_x > HEIGHT - SQUARE_SIZE) square_x = HEIGHT - SQUARE_SIZE;
         
-        // Eixo Y
-        if (square_y < 0) {
-            square_y = 0;
-        } else if (square_y > WIDTH - SQUARE_SIZE) {
-            square_y = WIDTH - SQUARE_SIZE;
-        }
+        if (square_y < 0) square_y = 0;
+        else if (square_y > WIDTH - SQUARE_SIZE) square_y = WIDTH - SQUARE_SIZE;
 
         // Atualiza o PWM dos LEDs
         if (pwm_enabled) {
-            if (abs(delta_x) > JOY_THRESHOLD) {
-                red_intensity = abs(delta_x) * PWM_WRAP / JOY_CENTER;
-            } else {
-                red_intensity = 0;
-            }
-            
-            if (abs(delta_y) > JOY_THRESHOLD) {
-                blue_intensity = abs(delta_y) * PWM_WRAP / JOY_CENTER;
-            } else {
-                blue_intensity = 0;
-            }
+            red_intensity = (abs(delta_x) > JOY_THRESHOLD) ? abs(delta_x) * PWM_WRAP / JOY_CENTER : 0;
+            blue_intensity = (abs(delta_y) > JOY_THRESHOLD) ? abs(delta_y) * PWM_WRAP / JOY_CENTER : 0;
 
             set_pwm_duty(LED_R, red_intensity);
             set_pwm_duty(LED_B, blue_intensity);
@@ -142,27 +154,16 @@ int main() {
             set_pwm_duty(LED_B, 0);
         }
 
-        // Lógica para os botões
-        if (!gpio_get(JOYSTICK_PB)) {
-            sleep_ms(50); // Debounce
-            if (!gpio_get(JOYSTICK_PB)) {
-                led_g_state = !led_g_state;
-                gpio_put(LED_G, led_g_state);
-                while (!gpio_get(JOYSTICK_PB)); // Espera o botão ser solto
-            }
-        }
-
-        if (!gpio_get(Botao_A)) {
-            sleep_ms(50); // Debounce
-            if (!gpio_get(Botao_A)) {
-                pwm_enabled = !pwm_enabled;
-                while (!gpio_get(Botao_A)); // Espera o botão ser solto
-            }
-        }
-
         // Atualiza o display
         ssd1306_fill(&ssd, false);
-
+        if (borda) {
+          ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);
+        } else {
+            ssd1306_line(&ssd, 3, 3, 122, 3, cor);
+            ssd1306_line(&ssd, 3, 60, 122, 60, cor);
+            ssd1306_line(&ssd, 3, 8, 3, 55, cor);
+            ssd1306_line(&ssd, 122, 8, 122, 55, cor);
+        }
         ssd1306_rect(&ssd, square_x, square_y, SQUARE_SIZE, SQUARE_SIZE, cor, !cor);
         ssd1306_send_data(&ssd);
 
